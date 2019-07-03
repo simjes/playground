a603bd7a1be9a6ed1d6648ecc1829757e741b806
Simon Jespersen <simjes91@me.com>
2019-07-03 15:50:43 +0200

    Add platforms util

---
+++ svelte-swing/src/utils/platforms.js
@@ -0,0 +1,28 @@
+const platforms = {
+  48: {
+    name: 'Playstation 4',
+    icon: '',
+  },
+  167: {
+    name: 'Playstation 5',
+    icon: '',
+  },
+  49: {
+    name: 'Xbox One',
+    icon: '',
+  },
+  37: {
+    name: 'Nintendo 3DS',
+    icon: '',
+  },
+  130: {
+    name: 'Nintendo Switch',
+    icon: '',
+  },
+  6: {
+    name: 'PC',
+    icon: '',
+  },
+};
+
+export default platforms;
